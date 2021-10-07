b3b410aaffdc94d1f379a17e5d25a3a1857f83ca
Ana Ribeiro <lidialira@alu.ufc.br>
2021-10-07 16:56:04 -0300

    Final screen

--- src/shared/views/Results/index.tsx
+++ src/shared/views/Results/index.tsx
@@ -1,11 +1,32 @@
+import {useNavigation} from '@react-navigation/core';
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {CATEGORIES} from '~/shared/constants/routes';
+import {ApplicationState} from '~/shared/store';
+import {setScoreAction} from '~/shared/store/ducks/user/actions';
 
 import * as S from './styles';
 
-const Results: React.FC = () => (
-  <S.Container>
-    <S.TextTitle>Results</S.TextTitle>
-  </S.Container>
-);
+const Results: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
 
+  const {score} = useSelector((state: ApplicationState) => state.user);
+
+  const replay = () => {
+    dispatch(setScoreAction(0));
+    navigation.navigate(CATEGORIES);
+  };
+  return (
+    <S.Container>
+      <S.InfoContainer>
+        <S.TextTitle>RESULTS</S.TextTitle>
+        <S.InfoText> Final Score: {score} </S.InfoText>
+        <S.Button onPress={() => replay()}>
+          <S.InfoText> Try again </S.InfoText>
+        </S.Button>
+      </S.InfoContainer>
+    </S.Container>
+  );
+};
 export default Results;
--- src/shared/views/Results/styles.ts
+++ src/shared/views/Results/styles.ts
@@ -1,12 +1,53 @@
+import {vs} from 'react-native-size-matters';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  background-color: ${({theme}) => theme.Colors.BACKGROUND};
 `;
 
 export const TextTitle = styled.Text`
   font-size: 20px;
-  font-family: OpenSans-Regular;
+  font-weight: bold;
+  font-family: 'OpenSans-Regular';
+  margin: 30px;
+`;
+
+export const InfoContainer = styled.TouchableOpacity.attrs(({theme}) => ({
+  shadowOpacity: 3,
+  shadowOffset: {height: 4, width: 5},
+  shadowColor: theme.Colors.gray_dark,
+  elevation: 7,
+}))`
+  padding: 15px 10px;
+  justify-content: center;
+  align-items: center;
+  width: ${vs(300)}px;
+  height: ${vs(300)}px;
+  background: ${({theme}) => theme.Colors.LIGHT_SKY_BLUE};
+  border-width: 1px;
+  border-color: ${({theme}) => theme.Colors.BLACK};
+  margin-bottom: ${vs(15)}px;
+  margin: ${vs(10)}px;
+  border-radius: 6px;
+  flex-direction: column;
+`;
+
+export const InfoText = styled.Text`
+  font-size: 18px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: ${vs(150)}px;
+  height: ${vs(50)}px;
+  align-items: center;
+  justify-content: center;
+  border-width: 1px;
+  border-radius: 10px;
+  margin: 30px;
+  padding: 10px;
+  border-color: ${({theme}) => theme.Colors.BLACK};
+  background-color: ${({theme}) => theme.Colors.BUTTON_COLOR};
 `;
